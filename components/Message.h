4704269170d02354cb85183e40a93670f1354cba
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-23 19:18:03 +0200

    Added Message class

---
+++ components/Message.h
@@ -0,0 +1,14 @@
+//
+// Created by s41m0n on 20/05/19.
+//
+
+#ifndef COOPERATIVEEDITOR_MESSAGE_H
+#define COOPERATIVEEDITOR_MESSAGE_H
+
+
+class Message {
+};
+
+
+
+#endif //COOPERATIVEEDITOR_MESSAGE_H
