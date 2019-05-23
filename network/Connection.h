064ccb41d36db41e0a574f7e2775f95bebf9485d
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-23 19:14:02 +0200

    Added Connection header

---
+++ network/Connection.h
@@ -0,0 +1,30 @@
+//
+// Created by s41m0n on 22/05/19.
+//
+
+#ifndef COOPERATIVEEDITOR_CONNECTION_H
+#define COOPERATIVEEDITOR_CONNECTION_H
+
+//
+// Connection.hpp
+// ~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/asio.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+#endif //COOPERATIVEEDITOR_CONNECTION_H
