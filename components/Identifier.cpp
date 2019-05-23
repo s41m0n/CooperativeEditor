6480de900c57b288504574bfe2bb4fae468eb166
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-23 19:17:52 +0200

    Added Identifier class

---
+++ components/Identifier.cpp
@@ -0,0 +1,6 @@
+//
+// Created by s41m0n on 17/05/19.
+//
+
+#include "Identifier.h"
+
---
+++ components/Identifier.h
@@ -0,0 +1,12 @@
+//
+// Created by s41m0n on 20/05/19.
+//
+
+#ifndef COOPERATIVEEDITOR_IDENTIFIER_H
+#define COOPERATIVEEDITOR_IDENTIFIER_H
+
+class Identifier {
+};
+
+
+#endif //COOPERATIVEEDITOR_IDENTIFIER_H
