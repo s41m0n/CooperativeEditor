ee1000aa48d3794f43beae36828de867344750b1
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-23 19:18:14 +0200

    Added Symbol class

---
+++ components/Symbol.cpp
@@ -0,0 +1,6 @@
+//
+// Created by s41m0n on 16/05/19.
+//
+
+#include "Symbol.h"
+
---
+++ components/Symbol.h
@@ -0,0 +1,13 @@
+//
+// Created by s41m0n on 20/05/19.
+//
+
+#ifndef COOPERATIVEEDITOR_SYMBOL_H
+#define COOPERATIVEEDITOR_SYMBOL_H
+
+
+class Symbol {
+};
+
+
+#endif //COOPERATIVEEDITOR_SYMBOL_H
