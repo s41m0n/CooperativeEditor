c37b7ab382cf8280a0209316e3cc704fbf242942
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:41:40 +0200

    Implemented getter methods for Symbol

--- components/Symbol.cpp
+++ components/Symbol.cpp
@@ -8,3 +8,12 @@ Symbol::Symbol(char character, int siteId, int digit, std::vector<int> &position
         : position(std::move(position)), id(siteId, digit), character(character){}
 
 Symbol::Symbol() : character('\0'), id(), position(std::vector<int>()){}
+
+const char Symbol::getChar() {
+    return this->character;
+}
+
+const std::vector<int> Symbol::getPos() {
+    return this->position;
+}
+
