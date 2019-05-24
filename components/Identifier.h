ea003be9624249195e9ca646a29944060b5a3626
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:38:52 +0200

    Completed header for Identifier

--- components/Identifier.h
+++ components/Identifier.h
@@ -5,7 +5,45 @@
 #ifndef COOPERATIVEEDITOR_IDENTIFIER_H
 #define COOPERATIVEEDITOR_IDENTIFIER_H
 
+
+// Forward declaration of class boost::serialization::access
+#pragma once
+namespace boost {
+    namespace serialization {
+        class access;
+    }
+}
+
+/**
+ * Identifier class, used to identify a symbol
+ *
+ * @author Simone Magnani - s41m0n
+ *
+ */
 class Identifier {
+
+private:
+    ///The editor specific ID
+    unsigned int editorId;
+    ///Unique digit inside the editor
+    unsigned int digit;
+    ///Class to access Boost to serialize this class
+    friend class boost::serialization::access;
+
+    ///Method called by Boost to serialize
+    template<typename Archive>
+    void serialize(Archive& ar, const unsigned version) {
+        ar & editorId & digit;
+    }
+
+public:
+    ///Classic constructor, given both the two parameter
+    Identifier(unsigned int editorId, unsigned int digit);
+    ///Constructor used to create a non-valid Identifier to be filled
+    Identifier();
+    ///Method to compare two identifier by their private variables
+    int compareTo(Identifier other);
+
 };
 
 
