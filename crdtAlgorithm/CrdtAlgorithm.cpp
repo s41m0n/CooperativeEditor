7f0a675f235b1a49b2a0581ace5f7193e8326044
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:36:23 +0200

    Implemented method findPositionErase in CrdtAlgorithm

--- crdtAlgorithm/CrdtAlgorithm.cpp
+++ crdtAlgorithm/CrdtAlgorithm.cpp
@@ -7,3 +7,28 @@
 int CrdtAlgorithm::generateIdBetween(int id1, int id2){
     return (id2 + id1) / 2;
 }
+
+int CrdtAlgorithm::findPositionErase(Symbol s, std::vector<Symbol>& symbols) {
+    int left = 0, mid, right = symbols.size() - 1;
+
+    // check if struct is empty or char is less than first char
+    if (symbols.empty() || s.compareTo(symbols[left]) < 0)
+        return 0;
+    else if (s.compareTo(symbols[right]) > 0)
+        return symbols.size();
+
+
+    // binary search
+    while (left + 1 < right) {
+        mid = std::floor(left + (right - left) / 2.0);
+
+        int comp = s.compareTo(symbols[mid]);
+        if (comp == 0)
+            return mid;
+        else if (comp < 0)
+            right = mid;
+        else
+            left = mid;
+    }
+    return -1;
+}
