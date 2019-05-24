7639a6def06fe9195fcdda4189546230e7f84e14
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:57:49 +0200

    Implemented constructor for NetworkServer

--- server/NetworkServer.cpp
+++ server/NetworkServer.cpp
@@ -2,4 +2,23 @@
 // Created by s41m0n on 16/05/19.
 //
 
+#include <spdlog/spdlog.h>
+#include <boost/asio.hpp>
+#include <fstream>
 #include "NetworkServer.h"
+#include "../network/Connection.h"
+#include "../crdtAlgorithm/CrdtAlgorithm.h"
+
+NetworkServer::NetworkServer(boost::asio::io_service& io_service, unsigned short port)
+: acceptor_(io_service,
+        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)), symbols() {
+
+    spdlog::debug("NetworkServer::Created NetworkServer");
+
+    //Create new connection and start listening asynchronously
+    connection_ptr new_conn(new Connection(acceptor_.get_io_service()));
+    acceptor_.async_accept(new_conn->socket(),
+        boost::bind(&NetworkServer::handle_accept, this,
+                    boost::asio::placeholders::error, new_conn));
+}
+
