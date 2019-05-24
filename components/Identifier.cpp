a12d8c13af39a6de09e261db1e305385ef4a31f8
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:40:54 +0200

    Implemented method compareTo for Identifier

--- components/Identifier.cpp
+++ components/Identifier.cpp
@@ -8,3 +8,19 @@ Identifier::Identifier(unsigned int editorId, unsigned int digit) : editorId(edi
 
 Identifier::Identifier() : editorId(-1), digit(-1){
 }
+
+int Identifier::compareTo(Identifier other) {
+    if (this->digit < other.digit) {
+        return -1;
+    } else if (this->digit > other.digit) {
+        return 1;
+    } else {
+        if (this->editorId < other.editorId) {
+            return -1;
+        } else if (this->editorId > other.editorId) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
