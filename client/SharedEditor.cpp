911780caae67aa1159ac73b9fa256cb66827b710
Simone Magnani <simonemagnani.96@gmail.com>
2019-05-24 02:51:55 +0200

    Implemented constructor for SharedEditor

--- client/SharedEditor.cpp
+++ client/SharedEditor.cpp
@@ -2,5 +2,28 @@
 // Created by s41m0n on 16/05/19.
 //
 
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <spdlog/spdlog.h>
+#include <fstream>
 #include "SharedEditor.h"
+#include "../server/NetworkServer.h"
+#include "../crdtAlgorithm/CrdtAlgorithm.h"
+
+SharedEditor::SharedEditor(boost::asio::io_service& io_service,
+const std::string& host, const std::string& service)
+: conn(io_service), editorId(-1), _counter(0) {
+
+    // Resolve the host name into an IP address.
+    boost::asio::ip::tcp::resolver resolver(io_service);
+    boost::asio::ip::tcp::resolver::query query(host, service);
+    boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
+
+    //Connect asynchronously
+    conn.socket().async_connect(endpoint,
+            boost::bind(&SharedEditor::handle_connect, this,
+        boost::asio::placeholders::error, ++endpoint_iterator));
+    spdlog::debug("SharedEditor?::Created");
+}
 
