c2a1591e1e219606a53a3b55e1e2a169be8e623b
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-01 12:50:50 +0200

    Created main for client

---
+++ mainclient.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <boost/serialization/export.hpp>
+#include <spdlog/spdlog.h>
+#include <boost/lexical_cast.hpp>
+#include <boost/thread.hpp>
+#include "client/SharedEditor.h"
+
+
+BOOST_CLASS_EXPORT(CrdtMessage)
+BOOST_CLASS_EXPORT(RequestMessage)
+BOOST_CLASS_EXPORT(FileContentMessage)
+BOOST_CLASS_EXPORT(FilesListingMessage)
+
+int main() {
+    const std::string host("127.0.0.1");
+    const std::string port("3000");
+
+    std::vector<std::pair<int, char>> toInsert({{0,'c'}, {1, 'i'}, {2, 'a'}, {3, 'o'}, {4, ' '},
+                                                       {5,'s'}, {6, 'i'}, {7, 'm'}, {8, 'o'},
+                                                       {4, ' '}, {5, 's'}, {6, 'c'}, {7, 'u'}, {8, 's'}, {9, 'a'}});
+    std::vector<int> toDelete;
+
+    //Setting LogLevel=debug
+    spdlog::set_level(spdlog::level::debug);
+
+
+    boost::asio::io_service io_service;
+
+    std::shared_ptr<boost::asio::io_service::work> work(new boost::asio::io_service::work(io_service));
+    boost::thread(boost::bind(&boost::asio::io_service::run, &io_service));
+
+    std::shared_ptr<SharedEditor> client(new SharedEditor(io_service, host, port));
+
+    std::for_each(toInsert.begin(), toInsert.end(), [&client](std::pair<int, char> pair) -> void {
+        client->localInsert(pair.first, pair.second);
+    });
+
+    std::for_each(toDelete.begin(), toDelete.end(), [&client](int index) -> void {
+        client->localErase(index);
+    });
+
+}
