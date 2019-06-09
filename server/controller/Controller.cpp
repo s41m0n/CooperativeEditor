abe37153002e432b4f2ce187d5f74c8198f20a1f
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-10 00:24:32 +0200

    Modified server controller, removed unused methods

--- server/controller/Controller.cpp
+++ server/controller/Controller.cpp
@@ -5,268 +5,222 @@
 #include <spdlog/spdlog.h>
 #include <fstream>
 #include <boost/thread.hpp>
+#include <crdtAlgorithm/CrdtAlgorithm.h>
 #include "Controller.h"
 
-Controller::Controller(boost::asio::io_service& io_service, unsigned short port)
-: acceptor_(io_service,
-        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)), openedFiles(), idGenerator(0) {
-
-    spdlog::debug("NetworkServer::Created NetworkServer");
-
-    loadAllFileNames();
-
-    //Create new connection and start listening asynchronously
-    connection_ptr new_conn(new Connection(acceptor_.get_io_service()));
-    acceptor_.async_accept(new_conn->socket(),
-        boost::bind(&Controller::handle_accept, this,
-                    boost::asio::placeholders::error, new_conn));
+Controller::Controller(Model *model, unsigned short port) : model(model),
+    acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {
+  spdlog::debug("NetworkServer::Created NetworkServer");
 }
 
 Controller::~Controller() {
-    spdlog::debug("NetworkServer::Destroyed");
+  spdlog::debug("NetworkServer::Destroyed");
 }
 
-void Controller::handle_accept(const boost::system::error_code& e, connection_ptr& conn)
-{
-    if (!e) {
+void Controller::handle_accept(const boost::system::error_code &e, connection_ptr &conn) {
 
-        connections.insert(std::pair<int, std::pair<std::string, connection_ptr>>(idGenerator,
-                std::pair<std::string, connection_ptr>("",conn)));
+  if (!e) {
 
-        buffers.insert(std::pair<int, std::pair<boost::shared_ptr<BasicMessage>, boost::shared_ptr<BasicMessage>>>(
-                idGenerator, std::pair<boost::shared_ptr<BasicMessage>, boost::shared_ptr<BasicMessage>>()));
+    auto id = model->generateEditorId();
 
-        //Connected successfully, send editorId and start reading & listening recursively
-        spdlog::debug("NetworkServer::Connected Shared Editor{}", idGenerator);
-        buffers[idGenerator].second = boost::make_shared<BasicMessage>(Type::CONNECT, idGenerator);
-        conn->async_write(buffers[idGenerator].second,
-                          boost::bind(&Controller::handle_write, this,
-                                      boost::asio::placeholders::error, idGenerator));
+    std::lock_guard<std::mutex> guard(connectionsMutex);
+    connections.insert(std::pair<int, connection_ptr>(id, conn));
 
-        //Start a thread to dispatch messages
-        if(connections.size() == 2)
-            boost::thread(boost::bind(&Controller::dispatch, this));
+    //Connected successfully, send editorId and start reading & listening recursively
+    spdlog::debug("NetworkServer::Connected Shared Editor{}", id);
+    auto msg = new BasicMessage(Type::CONNECT, id);
+    conn->async_write(*msg,
+                      boost::bind(&Controller::handle_write, this,
+                                  boost::asio::placeholders::error, id, msg));
 
+    //Start a thread to dispatch messages
+    if (connections.size() == 2)
+      boost::thread(boost::bind(&Controller::dispatch, this));
 
-        idGenerator++;
-
-        // Start an accept operation for a new connection.
-        connection_ptr new_conn(new Connection(acceptor_.get_io_service()));
-        acceptor_.async_accept(new_conn->socket(),
-                               boost::bind(&Controller::handle_accept, this,
-                                           boost::asio::placeholders::error, new_conn));
-    }
-    else
-        spdlog::error("NetworkServer::Connect error -> {}", e.message());
-
+    // Start an accept operation for a new connection.
+    connection_ptr new_conn(new Connection(acceptor_.get_io_service()));
+    acceptor_.async_accept(new_conn->socket(),
+                           boost::bind(&Controller::handle_accept, this,
+                                       boost::asio::placeholders::error, new_conn));
+  } else {
+    spdlog::error("NetworkServer::Connect error -> {}", e.message());
+  }
 }
 
-void Controller::handle_read(const boost::system::error_code& e, int connId) {
+void Controller::handle_read(const boost::system::error_code &e, int connId, BasicMessage* msg) {
 
-    if (!e) {
-        spdlog::debug("NetworkServer::Received Message (type={}) from SharedEditor{}", buffers[connId].first->getMsgType(), connId);
+  if (!e) {
 
-        switch (buffers[connId].first->getMsgType()) {
+    spdlog::debug("NetworkServer::Received Message (type={}) from SharedEditor{}", msg->getMsgType(), connId);
 
-            case Type::INSERT : {
-                boost::shared_ptr<CrdtMessage> converted = boost::static_pointer_cast<CrdtMessage>(buffers[connId].first);
-                std::string filename = connections[connId].first;
+    switch (msg->getMsgType()) {
 
-                std::lock_guard<std::mutex> guard(symbolsMutes);
-                CrdtAlgorithm::remoteInsert(converted->getSymbol(), openedFiles[filename]);
+      case Type::INSERT : {
+        auto converted = dynamic_cast<CrdtMessage*>(msg);
 
-                writeOnFile(filename);
+        model->userInsert(connId, converted->getSymbol());
 
-                std::lock_guard<std::mutex> guard2(queueMutex);
-                messages.push(std::move(converted));
-                break;
-            }
-            case Type::ERASE : {
-                boost::shared_ptr<CrdtMessage> converted = boost::static_pointer_cast<CrdtMessage>(buffers[connId].first);
-                std::string filename = connections[connId].first;
+        std::lock_guard<std::mutex> guard2(queueMutex);
+        messages.push(converted);
 
-                std::lock_guard<std::mutex> guard(symbolsMutes);
-                CrdtAlgorithm::remoteErase(converted->getSymbol(), openedFiles[filename]);
-
-                writeOnFile(filename);
-
-                std::lock_guard<std::mutex> guard2(queueMutex);
-                messages.push(std::move(converted));
-                break;
-            }
-            case Type::CREATE : {
-                std::string filename = boost::static_pointer_cast<RequestMessage>(buffers[connId].first)->getFilename();
-
-                if(availableFiles.find(filename) != std::string::npos) {
-
-                    buffers[connId].second = boost::make_shared<RequestMessage>(Type::FILEKO, connId, filename);
-                    connections[connId].second->async_write(buffers[connId].second,
-                            boost::bind(&Controller::handle_write, this,
-                                    boost::asio::placeholders::error, connId));
-                } else {
-
-                    connections[connId].first = filename;
-                    availableFiles.append(filename + ";");
-
-                    openedFiles.insert(std::pair<std::string, std::vector<Symbol>>(filename, std::vector<Symbol>()));
-
-                    buffers[connId].second = boost::make_shared<RequestMessage>(Type::FILEOK, connId, connections[connId].first);
-                    connections[connId].second->async_write(buffers[connId].second,
-                            boost::bind(&Controller::handle_write, this,
-                                    boost::asio::placeholders::error, connId));
-                }
-
-                return;
-            }
-            case Type::OPEN : {
-                std::string filename = boost::static_pointer_cast<RequestMessage>(buffers[connId].first)->getFilename();
-
-                if(openedFiles.empty() || openedFiles.find(filename) == openedFiles.end()) {
-
-                    openedFiles.insert(
-                            std::pair<std::string, std::vector<Symbol>>(filename, std::vector<Symbol>()));
-
-                    restoreFromFile(filename);
-
-                    connections[connId].first = filename;
-                    buffers[connId].second = boost::make_shared<RequestMessage>(Type::FILEOK, connId, filename);
-                    connections[connId].second->async_write(buffers[connId].second,
-                            boost::bind(&Controller::handle_write, this,
-                                    boost::asio::placeholders::error, connId));
-                } else {
-
-                    buffers[connId].second = boost::make_shared<RequestMessage>(Type::FILEKO, connId, filename);
-                    connections[connId].second->async_write(buffers[connId].second,
-                            boost::bind(&Controller::handle_write, this,
-                                    boost::asio::placeholders::error, connId));
-                }
-                return;
-            }
-            default :
-                throw std::runtime_error("NetworkServer::Should never read different types of Message");
-
-        }
-
-        buffers[connId].first = boost::make_shared<CrdtMessage>();
-        connections[connId].second->async_read(buffers[connId].first,
+        auto newMsg = new CrdtMessage();
+        connections[connId]->async_read(*newMsg,
                 boost::bind(&Controller::handle_read, this,
-                        boost::asio::placeholders::error, connId));
-    } else {
+                        boost::asio::placeholders::error, connId, newMsg));
+        return;
+      }
+      case Type::ERASE : {
+        auto converted = dynamic_cast<CrdtMessage*>(msg);
 
-        buffers.erase(connId);
-        connections.erase(connId);
-        spdlog::error("NetworkServer::Read error -> {}", e.message());
+        model->userErase(connId, converted->getSymbol());
+
+        std::lock_guard<std::mutex> guard2(queueMutex);
+        messages.push(converted);
+
+        auto newMsg = new CrdtMessage();
+        connections[connId]->async_read(*newMsg,
+                boost::bind(&Controller::handle_read, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        return;
+      }
+      case Type::CREATE : {
+        std::string filename = dynamic_cast<RequestMessage*>(msg)->getFilename();
+
+        if (model->createFileByUser(connId, filename)) {
+
+          auto newMsg = new RequestMessage(Type::FILEOK, connId, filename);
+          connections[connId]->async_write(*newMsg,
+                                           boost::bind(&Controller::handle_write, this,
+                                                       boost::asio::placeholders::error, connId, newMsg));
+        } else {
+
+          auto newMsg = new RequestMessage(Type::FILEKO, connId, filename);
+          connections[connId]->async_write(*newMsg,
+                                           boost::bind(&Controller::handle_write, this,
+                                                       boost::asio::placeholders::error, connId, newMsg));
+        }
+
+        break;
+      }
+      case Type::OPEN : {
+        std::string filename = dynamic_cast<RequestMessage*>(msg)->getFilename();
+
+        if (model->openFileByUser(connId, filename)) {
+
+          auto newMsg = new RequestMessage(Type::FILEOK, connId, filename);
+          connections[connId]->async_write(*newMsg,
+                  boost::bind(&Controller::handle_write, this,
+                          boost::asio::placeholders::error, connId, newMsg));
+        } else {
+
+          auto newMsg = new RequestMessage(Type::FILEKO, connId, filename);
+          connections[connId]->async_write(*newMsg,
+                  boost::bind(&Controller::handle_write, this,
+                          boost::asio::placeholders::error, connId, newMsg));
+        }
+        break;
+      }
+      default :
+        throw std::runtime_error("NetworkServer::Should never read different types of Message");
     }
+  } else {
+    std::lock_guard<std::mutex> guard2(connectionsMutex);
+    connections.erase(connId);
+    spdlog::error("NetworkServer::Read error -> {}", e.message());
+  }
+
+  delete msg;
 }
 
-void Controller::handle_write(const boost::system::error_code& e, int connId)
-{
+void Controller::handle_write(const boost::system::error_code &e, int connId, BasicMessage* msg) {
 
-    if(!e) {
+  if (!e) {
 
-        spdlog::debug("NetworkServer::Sent Message (type={}) to SharedEditor{}", buffers[connId].second->getMsgType(), connId);
-        //Write successfully
-        switch (buffers[connId].second->getMsgType()) {
+    spdlog::debug("NetworkServer::Sent Message (type={}) to SharedEditor{}", msg->getMsgType(), connId);
 
-            case Type::CONNECT : {
-                buffers[connId].second = boost::make_shared<FilesListingMessage>(Type::LISTING, connId, availableFiles);
-                connections[connId].second->async_write(buffers[connId].second,
-                        boost::bind(&Controller::handle_write, this,
-                                boost::asio::placeholders::error, connId));
-                break;
-            }
-            case Type::LISTING : {
-                buffers[connId].first = boost::make_shared<RequestMessage>();
-                connections[connId].second->async_read(buffers[connId].first,
-                        boost::bind(&Controller::handle_read, this,
-                                boost::asio::placeholders::error, connId));
-                break;
-            }
-            case Type::FILEOK : {
-                buffers[connId].second = boost::make_shared<FileContentMessage>(Type::CONTENT, connId, openedFiles[connections[connId].first]);
-                connections[connId].second->async_write(buffers[connId].second ,
-                        boost::bind(&Controller::handle_write, this,
-                                boost::asio::placeholders::error, connId));
-                break;
-            }
-            case Type::FILEKO : {
-                buffers[connId].first = boost::make_shared<RequestMessage>();
-                connections[connId].second->async_read(buffers[connId].first,
-                        boost::bind(&Controller::handle_read, this,
-                                boost::asio::placeholders::error, connId));
-                break;
-            }
-            case Type::CONTENT : {
-                buffers[connId].first = boost::make_shared<CrdtMessage>();
-                connections[connId].second->async_read(buffers[connId].first,
-                        boost::bind(&Controller::handle_read, this,
-                                boost::asio::placeholders::error, connId));
+    switch (msg->getMsgType()) {
 
-                break;
-            }
-            default:
-                throw std::runtime_error("NetworkServer::Should never write other types of Message");
-        }
-    } else {
-
-        buffers.erase(connId);
-        connections.erase(connId);
-        spdlog::error("NetworkServer::Write error -> {}", e.message());
+      case Type::CONNECT : {
+        auto fileList = model->getAvailableFiles();
+        auto newMsg = new FilesListingMessage(Type::LISTING, connId, fileList);
+        connections[connId]->async_write(*newMsg,
+                boost::bind(&Controller::handle_write, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        break;
+      }
+      case Type::LISTING : {
+        auto newMsg = new RequestMessage();
+        connections[connId]->async_read(*newMsg,
+                boost::bind(&Controller::handle_read, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        break;
+      }
+      case Type::FILEOK : {
+        auto symbolList = model->getFileSymbolList(connId);
+        auto newMsg = new FileContentMessage(Type::CONTENT, connId, symbolList);
+        connections[connId]->async_write(*newMsg,
+                boost::bind(&Controller::handle_write, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        break;
+      }
+      case Type::FILEKO : {
+        auto newMsg = new RequestMessage();
+        connections[connId]->async_read(*newMsg,
+                boost::bind(&Controller::handle_read, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        break;
+      }
+      case Type::CONTENT : {
+        auto newMsg = new CrdtMessage();
+        connections[connId]->async_read(*newMsg,
+                boost::bind(&Controller::handle_read, this,
+                        boost::asio::placeholders::error, connId, newMsg));
+        break;
+      }
+      case Type::INSERT : return;
+      case Type::ERASE : return;
+      default: throw std::runtime_error("NetworkServer::Should never write other types of Message");
     }
+  } else {
+    std::lock_guard<std::mutex> guard2(connectionsMutex);
+    connections.erase(connId);
+    spdlog::error("NetworkServer::Write error -> {}", e.message());
+  }
 
+  delete msg;
 }
 
 void Controller::dispatch() {
 
-    while(connections.size() >= 2) {
+  while (connections.size() >= 2) {
 
-        std::lock_guard<std::mutex> guard1(this->queueMutex);
+    std::lock_guard<std::mutex> guard1(queueMutex);
 
-        for(int i=0, size=this->messages.size(); i<size; i++) {
+    for (int i = 0, size = this->messages.size(); i < size; i++) {
 
-            auto msg = this->messages.front();
-            std::lock_guard<std::mutex> guard2(this->connectionMapMutex);
+      auto msg = this->messages.front();
+      std::lock_guard<std::mutex> guard2(connectionsMutex);
 
-            for(std::pair<int, std::pair<std::string,connection_ptr>> pair : connections) {
+      for (auto& conn : connections) {
 
-                if(pair.first != msg->getEditorId() && pair.second.second->socket().is_open())
-                    pair.second.second->async_write(msg,
-                                                    boost::bind(&Controller::handle_write, this,
-                                                                boost::asio::placeholders::error, pair.first));
-            }
+        if (conn.first != msg->getEditorId())
+          conn.second->async_write(*msg,
+                  boost::bind(&Controller::handle_write, this,
+                          boost::asio::placeholders::error, conn.first, msg));
+      }
 
-            this->messages.pop();
-        }
+      this->messages.pop();
+      delete msg;
     }
+  }
 }
 
-void Controller::writeOnFile(std::string& filename) {
-    std::ofstream file{filename + ".crdt"};
-    boost::archive::text_oarchive oa{file};
-    oa << openedFiles[filename];
-}
 
-void Controller::restoreFromFile(std::string& filename) {
-    std::ifstream file{filename + ".crdt"};
-    boost::archive::text_iarchive oa{file};
-    oa >> openedFiles[filename];
-}
-
-void Controller::loadAllFileNames() {
-
-    boost::filesystem::recursive_directory_iterator it(boost::filesystem::current_path());
-    boost::filesystem::recursive_directory_iterator end;
-
-    while(it != end) {
-
-        if(boost::filesystem::is_regular_file(*it) && it->path().extension() == ".crdt") {
-            std::string toAdd = it->path().filename().string();
-            size_t index = toAdd.find_last_of('.');
-            toAdd = toAdd.substr(0, index);
-            availableFiles.append(toAdd + ";");
-        }
-        ++it;
-
-    }
-
+int Controller::start() {
+  //Create new connection and start listening asynchronously
+  connection_ptr new_conn(new Connection(acceptor_.get_io_service()));
+  acceptor_.async_accept(new_conn->socket(),
+                         boost::bind(&Controller::handle_accept, this,
+                                     boost::asio::placeholders::error, new_conn));
+  return io_service.run();
 }
--- server/controller/Controller.h
+++ server/controller/Controller.h
@@ -7,16 +7,13 @@
 
 
 #include <queue>
-#include <set>
 #include <map>
-#include <vector>
 #include <string>
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/filesystem.hpp>
-#include "../components/Message.h"
-#include "../network/Connection.h"
-#include "../crdtAlgorithm/CrdtAlgorithm.h"
+#include "components/Message.h"
+#include "network/Connection.h"
+#include "../model/Model.h"
 
 /**
  * NetworkServer class, the server in this architecture
@@ -26,32 +23,26 @@
 class Controller {
 
 private:
-    std::map<int, std::pair<boost::shared_ptr<BasicMessage>, boost::shared_ptr<BasicMessage>>> buffers;
-    ///The map of the connection, containing the editorId and the pointer to the connection
-    std::map<std::string, std::vector<Symbol>> openedFiles;
+    ///The model instance
+    Model* model;
+
+    ///The IO SERVICE
+    boost::asio::io_service io_service;
+
     ///Map of connection ID, filename required by user and Connection ptr
-    std::map<int, std::pair<std::string, connection_ptr>> connections;
+    std::map<int, connection_ptr> connections;
+
     ///The queue containing all the messages
-    std::queue<boost::shared_ptr<CrdtMessage>> messages;
+    std::queue<CrdtMessage*> messages;
+
     ///The mutex used for the message queue
     std::mutex queueMutex;
-    ///The mutex used to update the symbols array
-    std::mutex symbolsMutes;
-    ///The mutex used for the connections map
-    std::mutex connectionMapMutex;
-    ///Editor Id unique generator
-    unsigned int idGenerator;
-    ///Set of all available files
-    std::string availableFiles;
+
+    ///The mutex used for the connection map
+    std::mutex connectionsMutex;
+
     /// The acceptor object used to accept incoming socket connections.
     boost::asio::ip::tcp::acceptor acceptor_;
-
-public:
-    ///Class constructor, given an io_service and a port
-    Controller(boost::asio::io_service &io_service, unsigned short port);
-
-    ///Class destructor (actually used to debug)
-    ~Controller();
 
     ///Method to dispatch messages
     void dispatch();
@@ -60,20 +51,20 @@ public:
     void handle_accept(const boost::system::error_code &e, connection_ptr &conn);
 
     /// Handle completion of a read operation.
-    void handle_read(const boost::system::error_code &e, int connId);
+    void handle_read(const boost::system::error_code &e, int connId, BasicMessage* msg);
 
     /// Handle completion of a write operation.
-    void handle_write(const boost::system::error_code &e, int connId);
+    void handle_write(const boost::system::error_code &e, int connId, BasicMessage* msg);
 
-    ///Method to write on file the respective sequence of symbols
-    void writeOnFile(std::string& filename);
+public:
+    ///Class constructor, given an io_service and a port
+    Controller(Model* model, unsigned short port);
 
-    ///Method to restore from file the respective sequence of symbols
-    void restoreFromFile(std::string& filename);
+    ///Class destructor (actually used to debug)
+    ~Controller();
 
-    ///Method to perform directory listing for file with certain extension
-    void loadAllFileNames();
-
+    ///Method to start the IO
+    int start();
 };
 
 #endif //COOPERATIVEEDITOR_CONTROLLER_H
