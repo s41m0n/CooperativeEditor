9ec31ebeb7e1f522ceb0236a52c7e903da3f8f57
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-10 00:24:47 +0200

    Added server model

---
+++ server/model/Model.cpp
@@ -0,0 +1,113 @@
+//
+// Created by s41m0n on 07/06/19.
+//
+
+#include <spdlog/spdlog.h>
+#include <boost/filesystem.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include "Model.h"
+#include "crdtAlgorithm/CrdtAlgorithm.h"
+
+Model::Model() : idGenerator(0){
+
+  spdlog::debug("NetworkServer::Created Model");
+
+  //Loading all files name in the directory
+  boost::filesystem::recursive_directory_iterator it(boost::filesystem::current_path());
+  boost::filesystem::recursive_directory_iterator end;
+
+  while(it != end) {
+
+    if(boost::filesystem::is_regular_file(*it) && it->path().extension() == ".crdt") {
+      std::string toAdd = it->path().filename().string();
+      size_t index = toAdd.find_last_of('.');
+      toAdd = toAdd.substr(0, index);
+      availableFiles.append(toAdd + ";");
+    }
+    ++it;
+  }
+}
+
+Model::~Model() {
+
+  spdlog::debug("NetworkServer::Destroyed Model");
+
+}
+
+void Model::storeFileSymbols(std::string& filename) {
+  std::ofstream file{filename + ".crdt"};
+  boost::archive::text_oarchive oa{file};
+  oa << openedFiles[filename];
+}
+
+void Model::loadFileSymbols(std::string& filename) {
+  std::ifstream file{filename + ".crdt"};
+  boost::archive::text_iarchive oa{file};
+  oa >> openedFiles[filename];
+}
+
+unsigned int Model::generateEditorId() {
+  return idGenerator++;
+}
+
+void Model::userInsert(int connId, Symbol symbol) {
+
+  auto filename = usersFile[connId];
+
+  std::lock_guard<std::mutex> guard(openedFilesMutexes[filename]);
+
+  CrdtAlgorithm::remoteInsert(std::move(symbol), openedFiles[filename]);
+
+  storeFileSymbols(filename);
+}
+
+void Model::userErase(int connId, Symbol symbol) {
+
+  std::string filename = usersFile[connId];
+
+  std::lock_guard<std::mutex> guard(openedFilesMutexes[filename]);
+
+  CrdtAlgorithm::remoteErase(std::move(symbol), openedFiles[filename]);
+
+  storeFileSymbols(filename);
+
+}
+
+bool Model::createFileByUser(int connId, std::string& filename) {
+  std::lock_guard<std::mutex> guard(openedFilesMapMutex);
+
+  if (availableFiles.find(filename) != std::string::npos) {
+    return false;
+  } else {
+    availableFiles.append(filename + ";");
+    usersFile[connId] = filename;
+    openedFiles.emplace(filename, std::vector<Symbol>());
+    openedFilesMutexes.emplace(filename, std::mutex());
+    return true;
+  }
+}
+
+bool Model::openFileByUser(int connId, std::string& filename) {
+  std::lock_guard<std::mutex> guard(openedFilesMapMutex);
+
+  if(availableFiles.empty() || availableFiles.find(filename))
+    return false;
+  else {
+    usersFile[connId] = filename;
+    if(openedFiles.find(filename) == openedFiles.end()) {
+      openedFiles.emplace(filename, std::vector<Symbol>());
+      openedFilesMutexes.emplace(filename, std::mutex());
+      loadFileSymbols(filename);
+    }
+    return true;
+  }
+}
+
+const std::string Model::getAvailableFiles() {
+  return availableFiles;
+}
+
+const std::vector<Symbol> Model::getFileSymbolList(int connId) {
+  return openedFiles[usersFile[connId]];
+}
---
+++ server/model/Model.h
@@ -0,0 +1,69 @@
+//
+// Created by s41m0n on 07/06/19.
+//
+
+#ifndef COOPERATIVEEDITOR_MODEL_H
+#define COOPERATIVEEDITOR_MODEL_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include "../../components/Symbol.h"
+
+class Model {
+
+private:
+    ///The map of the connection, containing the editorId and the pointer to the connection
+    std::map<std::string, std::vector<Symbol>> openedFiles;
+
+    ///The mutexes for the opened files
+    std::map<std::string, std::mutex> openedFilesMutexes;
+
+    ///The mutex for the files mutex map
+    std::mutex openedFilesMapMutex;
+
+    ///The map of the current file for each user
+    std::map<int, std::string> usersFile;
+
+    ///Editor Id unique generator
+    std::atomic<int> idGenerator;
+
+    ///Set of all available files
+    std::string availableFiles;
+
+    ///Method to write on file the respective sequence of symbols
+    void storeFileSymbols(std::string& filename);
+
+    ///Method to restore from file the respective sequence of symbols
+    void loadFileSymbols(std::string& filename);
+
+public:
+    ///Classic constructor
+    Model();
+
+    ///Classic destructor
+    ~Model();
+
+    ///Method to generate a new editor id
+    unsigned int generateEditorId();
+
+    void userInsert(int connId, Symbol symbol);
+
+    void userErase(int connId, Symbol symbol);
+
+    ///Method called when a user requests to create a file
+    bool createFileByUser(int connId, std::string& filename);
+
+    ///Method called when a user requests to open a file
+    bool openFileByUser(int connId, std::string& filename);
+
+    ///Returns the list (string) of all available files
+    const std::string getAvailableFiles();
+
+    ///Returns the list of symbols for the file the user connId has requested
+    const std::vector<Symbol> getFileSymbolList(int connId);
+
+};
+
+
+#endif //COOPERATIVEEDITOR_MODEL_H
