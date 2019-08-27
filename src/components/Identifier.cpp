329727b75223c1b9084825c527ac2a5b627cf271
Simone Magnani <simonemagnani.96@gmail.com>
2019-08-27 22:58:16 +0200

    Added toString method and operator overload for serialization

--- src/components/Identifier.cpp
+++ src/components/Identifier.cpp
@@ -7,17 +7,34 @@
 Identifier::Identifier(unsigned int editorId, unsigned int digit) : editorId(editorId), digit(digit) {}
 
 int Identifier::compareTo(Identifier other) {
-    if (this->digit < other.digit) {
-        return -1;
-    } else if (this->digit > other.digit) {
-        return 1;
+  if (this->digit < other.digit) {
+    return -1;
+  } else if (this->digit > other.digit) {
+    return 1;
+  } else {
+    if (this->editorId < other.editorId) {
+      return -1;
+    } else if (this->editorId > other.editorId) {
+      return 1;
     } else {
-        if (this->editorId < other.editorId) {
-            return -1;
-        } else if (this->editorId > other.editorId) {
-            return 1;
-        } else {
-            return 0;
-        }
+      return 0;
     }
+  }
+}
+
+std::string Identifier::toString(int level) {
+  return std::string(std::string(level, '\t') + "Identifier{\n" +
+                     std::string(level + 1, '\t') + "editorId: " + std::to_string(editorId) + "\n" +
+                     std::string(level + 1, '\t') + "digit: " + std::to_string(digit) + "\n" +
+                     std::string(level, '\t') + "}");
+}
+
+QDataStream &operator<<(QDataStream &stream, const Identifier &val) {
+  stream << val.editorId << val.digit;
+  return stream;
+}
+
+QDataStream &operator>>(QDataStream &stream, Identifier &val) {
+  stream >> val.editorId >> val.digit;
+  return stream;
 }
--- src/components/Identifier.h
+++ src/components/Identifier.h
@@ -5,14 +5,8 @@
 #ifndef COOPERATIVEEDITOR_IDENTIFIER_H
 #define COOPERATIVEEDITOR_IDENTIFIER_H
 
-
-// Forward declaration of class boost::serialization::access
-#pragma once
-namespace boost {
-    namespace serialization {
-        class access;
-    }
-}
+#include <QtCore/QDataStream>
+#include <string>
 
 /**
  * Identifier class, used to identify a symbol
@@ -25,24 +19,28 @@ class Identifier {
 private:
     ///The editor specific ID
     unsigned int editorId;
+
     ///Unique digit inside the editor
     unsigned int digit;
-    ///Class to access Boost to serialize this class
-    friend class boost::serialization::access;
-
-    ///Method called by Boost to serialize
-    template<typename Archive>
-    void serialize(Archive& ar, const unsigned version) {
-        ar & editorId & digit;
-    }
 
 public:
     ///Classic constructor, given both the two parameter
     Identifier(unsigned int editorId, unsigned int digit);
+
     ///Constructor used to create a non-valid Identifier to be filled
     Identifier() = default;
+
     ///Method to compare two identifier by their private variables
     int compareTo(Identifier other);
+
+    ///Operator overload '<<' for Identifier when using QDataStream for serialization
+    friend QDataStream &operator<<(QDataStream &stream, const Identifier &val);
+
+    ///Operator overload '>>' for Identifier when using QDataStream for serialization
+    friend QDataStream &operator>>(QDataStream &stream, Identifier &val);
+
+    ///Method to print in human-readable format the Identifier
+    std::string toString(int level = 0);
 
 };
 
