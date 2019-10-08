50a4b3c7742142316d2ddbdc8f8a0ded378bb507
Francesco Pavan <pavanfrancesco87@gmail.com>
2019-10-08 11:19:44 +0200

    Added parsing function to retrieve server's files list

--- src/client/view/windows/Editor.cpp
+++ src/client/view/windows/Editor.cpp
@@ -1,9 +1,21 @@
 #include <spdlog/spdlog.h>
+#include <sstream>
 #include "Editor.h"
 
 Editor::Editor(QWidget *parent)  : QMainWindow(parent) {
 }
 
 void Editor::onFileListing(std::string files) {
-  //ricevo lista di file separati da ; e li mostro al client
+
+  //parsing of files string
+  std::replace(files.begin(), files.end(), ';', ' ');
+  std::vector<std::string> filesArray;
+  std::istringstream iss(files);
+  for(std::string s; iss >> s; ) {
+    filesArray.push_back(s);
+  }
+
+  //Ora devo creare un interfaccia per far scegliere all'utente i file
+
+
 }
